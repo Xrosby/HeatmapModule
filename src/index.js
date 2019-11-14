1f2b2ea1be93da96ed06055404dfa9506773b8c9
Xrosby <mads.sfg@gmail.com>
2019-11-14 12:52:43 +0100

    test data med square km som values

--- src/index.js
+++ src/index.js
@@ -1,38 +1,52 @@
 import h337 from 'heatmap.js'
+import HeatmapOverlay from 'leaflet-heatmap'
+let json = require('../data/countylocations.json');
+
 
 
 function initComponent() {
+  let cfg = {
+    "radius": 40,
+    "useLocalExtrema": true,
+    valueField: 'value'
+  };
+  let heatmapLayer = new HeatmapOverlay(cfg);
+
+  let baseLayer = L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }
+  )
+
+  let heatMapData = json.map(cLocation =>
+    ({
+      lat: cLocation.lat,
+      lng: cLocation.lng,
+      value: cLocation.square_km
+    })
+  )
+
+  console.log(heatMapData)
+
+  let min = Math.min(...heatMapData.map(location => location.value))
+  let max = Math.max(...heatMapData.map(location => location.value))
 
 
-  var heatmap = h337.create({
-    container: document.getElementById("map")
-  });
 
-  heatmap.setData({
-    max: 5,
-    data: [{
-        x: 100,
-        y: 550,
-        value: 100
-      },
-      {
-        x: 200,
-        y: 65,
-        value: 100
-      },
-      {
-        x: 120,
-        y: 315,
-        value: 25
-      },
-      {
-        x: 220,
-        y: 65,
-        value: 10
-      }
-    ]
-  });
+  // heatmap.setData(data);
+  let propertyHeatMap = new L.Map('map', {
+    center: new L.LatLng(32.78306, -96.80667),
+    zoom: 15,
+    layers: [baseLayer, heatmapLayer]
+  })
+  propertyHeatMap.setZoom(3.9)
 
+
+  heatmapLayer.setData({
+    min: min,
+    max: max,
+    data: heatMapData
+  })
 }
 
 initComponent()
